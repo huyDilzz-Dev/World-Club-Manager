999c8a33b31824dcaaddff9c516e29a7a383c7ed
huyDilzz-Dev <whoareyou172r@gmail.com>
2026-04-29 13:55:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-crequire("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const cors = require("cors");
 
@@ -14,6 +17,7 @@ app.use("/auth", require("./routes/auth"));
 app.use("/user", require("./routes/users"));
 app.use("/game", require("./routes/clubs"));
 
-app.listen(process.env.PORT, () =>
-  console.log("Server running on port " + process.env.PORT)
-);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+  console.log("Server running on port " + POR
+iPORn
