77dd1f961cdd287e250191c07eacd6921a0eef81
huyDilzz-Dev <whoareyou172r@gmail.com>
2026-05-01 00:14:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,4 +14,5 @@ app.get("/", (req,res)=>{
   res.send("OSM Clone API Running");
 });
 
-app.listen(3000, ()=> console.log("Server running"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running on " + PORT));
